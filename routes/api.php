874b89ad7697cafe49e27a055646f3b5419c6de0
mshoubasi <mr8wolf@gmail.com>
2023-01-28 14:12:37 +0200

    added routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CommentController;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\InteractionController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +18,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('/register',[AuthController::class,'register']);
+Route::post('/login',[AuthController::class,'login']);
+
+Route::group(['middleware' => ['auth:sanctum']], function(){
+
+    Route::get('/user',[AuthController::class, 'user']);
+    Route::post('/logout',[AuthController::class, 'logout']);
+    Route::apiResource('posts', PostController::class);
+    Route::get('/posts/{id}/comments',[CommentController::class, 'index']);
+    Route::post('/posts/{id}/comments',[CommentController::class, 'store']);
+    Route::put('/comments/{id}',[CommentController::class, 'update']);
+    Route::delete('/comments/{id}',[CommentController::class, 'destroy']);
+    Route::post('/posts/{id}/{Interact}', [InteractionController::class, 'post']);
+    Route::post('/comments/{id}/{Interact}', [InteractionController::class, 'comment']);
+
 });
